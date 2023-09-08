99f4713f6901df01a914692e753a2ae399b20fbb
Dac Dat <datnddev19@gmail.com>
2023-09-08 22:12:01 +0700

    ORM structure

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import configViewEngine from "./configs/viewEngine";
+import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
+import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -12,6 +13,8 @@ configViewEngine(app);
 //config body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+//connectDB
+connection();
 //init web routes
 initWebRoutes(app);
 
